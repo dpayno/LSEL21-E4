3326a1dc17c576f1110b04365a7a6a92dbed105e
monde38 <rolando.mandefro@alumnos.upm.es>
2021-03-07 16:34:54 +0100

    Añadir comentario de descripción al primer test

--- test_fsm_gps.py
+++ test_fsm_gps.py
@@ -4,6 +4,7 @@ from fsm_gps import FsmGps
 
 class TestStringMethods(unittest.TestCase):
 
+	"""TEST INIT ESTATE"""
 	def test_fsm_gps_initStateEqualToOff(self):
 		mi_fsm = FsmGps("mi_fsm")
 		mi_fsm.start()
