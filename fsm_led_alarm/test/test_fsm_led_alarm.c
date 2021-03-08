bd1cd52b19176811c341aafba14e8a980fc34023
Daniel Payno Zarceño <d.payno@alumnos.upm.es>
2021-03-08 20:33:10 +0100

    Añadir segundo test

--- fsm_led_alarm/test/test_fsm_led_alarm.c
+++ fsm_led_alarm/test/test_fsm_led_alarm.c
@@ -27,3 +27,12 @@ void test_fsm_led_alarm_FsmInitFillsStructureWithSomething(void)
     // Comprobamos que la funcion fsm_led_alarm_init ocupa espacio en memoriaWS
     TEST_ASSERT(memcmp(&zero, &f, sizeof(fsm_led_alarm_t)) != 0);
 }
+
+void test_fsm_led_alarm_FsmFireInitsInIdleState(void)
+{
+    fsm_led_alarm_t f;
+
+    fsm_led_alarm_init(&f, NULL);
+
+    TEST_ASSERT(f.fsm.current_state == LED_ALARM_IDLE);
+}
