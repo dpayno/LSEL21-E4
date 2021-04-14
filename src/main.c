62f89a475d9ac2538efefa0136b6409f592e215f
Daniel Payno Zarceño <d.payno@alumnos.upm.es>
2021-04-14 22:50:07 +0200

    Añade ejemplo echo MQTT

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include "esp_common.h"
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+
 #include "gpio.h"
 #include "esp_wifi_station_module.h"
+#include "MQTTClient.h"
 
-#define ap_name        "monde38"
-#define ap_password    "monde.38"
+#define AP_SSID     "vodafone8C84"
+#define AP_PASSWORD "XXXX"
 
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
@@ -49,19 +53,94 @@ uint32 user_rf_cal_sector_set(void)
     return rf_cal_sec;
 }
 
-void task_blink(void* ignore)
-{   
-
-    esp_wifi_station_module mod;
-    init_module_station( &mod, ap_name, ap_password);
-    connect_to_ap();
-    while(true) {}
-
-
-    vTaskDelete(NULL);
+void messageArrived(MessageData* data)
+{
+  int i =0;
+	printf("Message arrived on topic:%s \n",data->message->payload);
+  printf("Size of data %d\n",(int)data->message->payloadlen );
+  for(i=0; i<(int)data->message->payloadlen;i++)
+    printf("%c",((char *)data->message->payload)[i]);
 }
 
+void prvMQTTEchoTask(void *pvParameters)
+{
+	/* connect to m2m.eclipse.org, subscribe to a topic, send and receive messages regularly every 1 sec */
+	MQTTClient client;
+	Network network;
+	unsigned char sendbuf[80], readbuf[80];
+	int rc = 0,
+	count = 0;
+	MQTTPacket_connectData connectData = MQTTPacket_connectData_initializer;
 
+	pvParameters = 0;
+	NetworkInit(&network);
+	MQTTClientInit(&client, &network, 30000, sendbuf, sizeof(sendbuf), readbuf, sizeof(readbuf));
+
+	char* address = "192.168.0.36";
+	if ((rc = ConnectNetwork(&network, address, 8883)) != 0)
+		printf("Return code from network connect is %d\n", rc);
+
+#if defined(MQTT_TASK)
+	if ((rc = MQTTStartTask(&client)) != pdPASS)
+		printf("Return code from start tasks is %d\n", rc);
+#endif
+
+	connectData.MQTTVersion = 3;
+	connectData.clientID.cstring = "FreeRTOS_sample";
+
+	if ((rc = MQTTConnect(&client, &connectData)) != 0)
+		printf("Return code from MQTT connect is %d\n", rc);
+	else
+		printf("MQTT Connected\n");
+
+	if ((rc = MQTTSubscribe(&client, "FreeRTOS/sample/#", 2, messageArrived)) != 0)
+		printf("Return code from MQTT subscribe is %d\n", rc);
+
+	while (++count)
+	{
+		MQTTMessage message;
+		char payload[30];
+
+		message.qos = 1;
+		message.retained = 0;
+		message.payload = payload;
+		sprintf(payload, "message number %d", count);
+		message.payloadlen = strlen(payload);
+
+		if ((rc = MQTTPublish(&client, "FreeRTOS/sample/a", &message)) != 0)
+			printf("Return code from MQTT publish is %d\n", rc);
+#if !defined(MQTT_TASK)
+		if ((rc = MQTTYield(&client, 1000)) != 0)
+			printf("Return code from yield is %d\n", rc);
+#endif
+	}
+
+	/* do not return */
+}
+
+void wifi_task(void *pvParameters)
+{
+    struct station_config sta_config;
+    struct ip_info ip_config;
+    bzero(&sta_config, sizeof(struct station_config));
+
+    sprintf(sta_config.ssid, AP_SSID);
+    sprintf(sta_config.password, AP_PASSWORD);
+
+    wifi_station_set_config(&sta_config);
+    os_printf("%s\n", __func__);
+    wifi_get_ip_info(STATION_IF, &ip_config);
+
+    while(ip_config.ip.addr == 0) {
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        wifi_get_ip_info(STATION_IF, &ip_config);
+        printf("Connecting...\n");
+    }
+    printf("Connected\n");
+
+    xTaskCreate(prvMQTTEchoTask, "MQTTEcho0", 512, NULL, 3,	NULL);
+    vTaskDelete(NULL);
+}
 
 /******************************************************************************
  * FunctionName : user_init
@@ -71,5 +150,8 @@ void task_blink(void* ignore)
 *******************************************************************************/
 void user_init(void)
 {
-    xTaskCreate(&task_blink, "startup", 2048, NULL, 1, NULL);
+    wifi_set_opmode(STATION_MODE);
+    uint8_t wifiStatus = STATION_IDLE;
+    printf("SDK version:%s\n\r", system_get_sdk_version());
+    xTaskCreate(&wifi_task, "startup", 2048, NULL, 1, NULL);
 }
