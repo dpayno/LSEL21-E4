e4f513d8452c62289ce23510a0d3de51c2508914
Javier Calle <javiercalle97@gmail.com>
2021-05-14 19:04:02 +0200

    Actualizar version fsm(lectura ciclica)

--- lib/fsm_gps/fsm_gps.py
+++ lib/fsm_gps/fsm_gps.py
@@ -19,8 +19,6 @@ class FsmGps(object):
         """ Input shared variables 
         """
         self.flag_active = 0 # Flag find car
-        self.flag_find_car = 0 # Flag find car
-        self.flag_init_gps_record = 0 # Flag init GPS record
 
         """ Timeout GPS
         """
@@ -36,10 +34,7 @@ class FsmGps(object):
         self.machine.add_transition(trigger='start', source='init', dest='OFF')
         self.machine.add_transition('fire', 'OFF', 'ON', conditions=['is_active'])
         self.machine.add_transition('fire', 'ON', 'OFF', conditions=['is_not_active'])
-        self.machine.add_transition('fire', 'ON', 'ON', conditions=['find_car_and_not_assault_and_active'], after='send_gps_frame')
-        self.machine.add_transition('fire', 'ON', 'ASSAULT', conditions=['assault_and_active'], after='send_gps_frame_and_init_timer')
-        self.machine.add_transition('fire', 'ASSAULT', 'ASSAULT', conditions=['timeout_or_find_car_and_active'], after='send_gps_frame_and_init_timer')
-        self.machine.add_transition('fire', 'ASSAULT', 'OFF', conditions=['is_not_active'])
+        self.machine.add_transition('fire', 'ON', 'ON', conditions=['timeout_and_active'], after='send_gps_frame_and_init_timer')
 
     """ Guard functions
     """ 
@@ -49,30 +44,14 @@ class FsmGps(object):
     def is_not_active(self):
         return not self.flag_active
 
-    def find_car_and_not_assault_and_active(self):
-        return (self.flag_find_car and (not self.flag_init_gps_record) and self.flag_active)
-
-    def assault_and_active(self):
-        return self.flag_init_gps_record and self.flag_active
-
-    def timeout_or_find_car_and_active(self):
-        return ((int(dt.now().timestamp()) > self.timeout_gps) or self.flag_find_car) and self.flag_active
+    def timeout_and_active(self):
+        return ((int(dt.now().timestamp()) > self.timeout_gps) and self.flag_active)
 
     """ Output functions
     TODO: Modify GPS frame sending with GPS drivers
     """ 
-    def send_gps_frame(self):
-        msg = self.__get_gps()
-        self.sim868.gsm_post('ptsv2.com/t/fn719-1620149096/post', "PRUEBA_HEADER", str(msg))
-        print(f"Se ha enviado un nuevo frame GPS: {msg}")
 
     def send_gps_frame_and_init_timer(self):
-        msg = self.__get_gps()
-        self.sim868.gsm_post('ptsv2.com/t/fn719-1620149096/post', "PRUEBA_HEADER", str(msg))
-        print("Se ha enviado un nuevo frame GPS y se ha iniciado el timer: : {msg}")
-        self.timeout_gps = int(dt.now().timestamp()) + self.TIMEOUT_DEFAULT
-        
-    def __get_gps(self):
         gps_data = self.sim868.get_gps_data()
-        return "'UTC': {}, 'Latitude': {}, 'Longitude': {}, 'Altitude': {}, 'Speed': {}".format(gps_data["UTC"], gps_data["Latitude"], gps_data["Longitude"],gps_data["Altitude"],gps_data["Speed"])
+        print( "'UTC': {}, 'Latitude': {}, 'Longitude': {}, 'Altitude': {}, 'Speed': {}".format(gps_data["UTC"], gps_data["Latitude"], gps_data["Longitude"],gps_data["Altitude"],gps_data["Speed"]))
         
