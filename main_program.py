86c67edd996e67bc312be26a30f011612effc86d
Daniel Payno Zarceño <daniel.payno@tychetools.com>
2021-05-16 14:54:01 +0200

    Arreglar ejemplo

--- main_program.py
+++ main_program.py
@@ -1,12 +1,11 @@
 from lib.fsm_gps.fsm_gps import FsmGps
 from lib.fsm_gsm.fsm_gsm import FsmGsm
 from lib.data_manager.data_manager import DataManager
-# from lib.sim868_driver.sim868_driver import SIM868
-import time
+from lib.sim868_driver.sim868_driver import SIM868
 
-CLIENT_NAME = "gateway_client_test"
+CLIENT_NAME = "gateway_client"
 MQTT_PORT = 8883
-MQTT_BROKER = "192.168.0.38"
+MQTT_BROKER = "192.168.0.110"
 MQTT_PUBLISH_TOPIC = "alarm_status"
 MQTT_SUSCRIBE_TOPIC = "sensor_data"
 
@@ -14,25 +13,23 @@ MQTT_SUSCRIBE_TOPIC = "sensor_data"
 """
 def main():
 
-    # Start SIM868
+    # Create SIM868
     mi_sim868 = SIM868(4)
      # Create FSMs
     fsm_gps = FsmGps("fsm_gps", mi_sim868)
     fsm_gsm = FsmGsm("fsm_gsm")
-    fsm_gsm = "Hola"
-    fsm_gps = "Adios"
-
     # Create Data Manager
     data_manager = DataManager(fsm_gps, fsm_gsm, CLIENT_NAME, MQTT_PORT,
     		MQTT_BROKER, MQTT_PUBLISH_TOPIC, MQTT_SUSCRIBE_TOPIC)
 
-    # Init state
+    # Init FSMs
     fsm_gps.start()
     fsm_gsm.start()
+    # Init Data Manager
     data_manager.start()
 
+    # Infinite loop
     while True:
-        time.sleep(3)
         fsm_gps.fire()
         fsm_gsm.fire()
         data_manager.fire()
